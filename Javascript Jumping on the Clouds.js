f37cec2e4c58f8709f357e140934a93b04c150f8
Aurelie1234 <115810657+Aurelie1234@users.noreply.github.com>
2023-06-05 15:58:57 +0000

    Create Javascript Jumping on the Clouds.js

---
+++ Javascript Jumping on the Clouds.js
@@ -0,0 +1,17 @@
+function jumpingOnClouds(c) {
+    // Write your code here
+
+
+    let count = 0;
+    for( let i = 0; i < c.length-1; i++){
+        if( c[i + 2] != 1 ){
+            i++
+        }
+        count++
+    }
+    return count
+ 
+}
+
+
+}
